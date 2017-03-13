7f28b57acc15c81ddee2f0aa934e6973df122e0e
Pandolfi <lpandolfi@hpe.com>
2017-03-12 20:06:42 -0600

    Added: createCalendars function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'calendar-app',
@@ -11,10 +12,86 @@ export class AppComponent {
     public numberOfDays: number;
     public countryCode: string;
 
+    public calendar: any = [];
+
     constructor() {
     }
 
-    createCalendars() {
+    getMonthLabel(monthNumber: number) {
+        let monthLabel:string;
+        switch(monthNumber) {
+            case 0:
+                monthLabel = "January";
+                break;
+            case 1:
+                monthLabel = "February";
+                break;
+            case 2:
+                monthLabel = "March";
+                break;
+            case 3:
+                monthLabel = "April";
+                break;
+            case 4:
+                monthLabel = "May";
+                break;
+            case 5:
+                monthLabel = "June";
+                break;
+            case 6:
+                monthLabel = "July";
+                break;
+            case 7:
+                monthLabel = "August";
+                break;
+            case 8:
+                monthLabel = "September";
+                break;
+            case 9:
+                monthLabel = "October";
+                break;
+            case 10:
+                monthLabel = "November";
+                break;
+            case 11:
+                monthLabel = "December";
+                break;
 
+        }
+        return monthLabel;
+    }
+
+    createCalendars() {
+        console.log(this.startDate);
+        let currentDate = new Date(this.startDate);
+        let newMonth: Boolean = true;
+        let currentCalendar: number = 0;
+
+        for(let i=0; i<this.numberOfDays; i++)
+        {
+            if(newMonth) {
+                this.calendar.push({
+                    monthLabel: this.getMonthLabel(currentDate.getMonth()),
+                    yearLabel: currentDate.getFullYear(), 
+                    days: []
+                });
+                newMonth = false;
+            }
+
+            this.calendar[currentCalendar].days.push({
+                dayNumber: currentDate.getDate(),
+                visible: true,
+                holiday: false,
+                weekend: false,
+                dayOfWeek: currentDate.getDay()
+            });
+
+            currentDate.setDate(currentDate.getDate() + 1);
+            if(currentDate.getDate() == 1)
+                newMonth = true;
+
+        }
+
+        console.log(this.calendar);
     }
 }
